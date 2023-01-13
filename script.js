c9c102faf86f218778a650e124235e54cf546a40
mimoschi <116549931+mimoschi@users.noreply.github.com>
2023-01-13 22:42:39 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const petContainers = [...document.querySelectorAll('.pet-container')];
+const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
+const preBtn = [...document.querySelectorAll('.pre-btn')];
+
+petContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    nxtBtn[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth;
+    })
+
+    preBtn[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth;
+    })
+})
